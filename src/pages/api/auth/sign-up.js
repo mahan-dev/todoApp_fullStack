203b73f5a958ee788c882de05d0033ccb5a15cf5
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-01 14:11:43 +0330

    signup changes-6

--- src/pages/api/auth/sign-up.js
+++ src/pages/api/auth/sign-up.js
@@ -11,21 +11,21 @@ const handler = async (req, res) => {
     });
   try {
     await connectDb();
-    const { email, password } = req.body;
+    // const { email, password } = req.body;
 
-    const existUser = await User.findOne({ email: email });
-    if (existUser) {
-      res.status(401).json({
-        status: "Failed",
-        message: "User already exist's 😢",
-      });
-      return;
-    }
-    const encryptedPassword = await hashPassword(password);
-    const newUser = await User.create({
-      email: email,
-      password: encryptedPassword,
-    });
+    // const existUser = await User.findOne({ email: email });
+    // if (existUser) {
+    //   res.status(401).json({
+    //     status: "Failed",
+    //     message: "User already exist's 😢",
+    //   });
+    //   return;
+    // }
+    // const encryptedPassword = await hashPassword(password);
+    // const newUser = await User.create({
+    //   email: email,
+    //   password: encryptedPassword,
+    // });
 
     return res.status(201).json({
       status: "Success",
