baf9c01df34e4d135455644db78c5fa1fd50f2cf
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-01 15:50:24 +0330

    signup changes-12

--- src/pages/api/auth/sign-up.js
+++ src/pages/api/auth/sign-up.js
@@ -1,18 +1,36 @@
+// import User from "@/models/User";
+// import { hashPassword } from "@/utils/auth";
 import connectDb from "@/utils/ConnectDb";
 
 const handler = async (req, res) => {
   const Post = req.method === "POST";
   if (!Post)
-    return res.status(405).json({
+    return res.status(405).json({ 
       status: "Failed",
       message: "method not allowed",
     });
   try {
     await connectDb();
+    // const { email, password } = req.body;
 
-    return res.status(200).json({
+    // const existUser = await User.findOne({ email: email });
+    // if (existUser) {
+    //   res.status(401).json({
+    //     status: "Failed",
+    //     message: "User already exist's 😢",
+    //   });
+    //   return;
+    // }
+    // const encryptedPassword = await hashPassword(password);
+    // const newUser = await User.create({
+    //   email: email,
+    //   password: encryptedPassword,
+    // });
+
+    return res.status(201).json({
       status: "Success",
-      message: "DB connected successfully",
+      message: "Successfully created ✔",
+      // data: newUser,
     });
   } catch (err) {
     console.error("DB Error:", err);
