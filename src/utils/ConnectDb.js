5c2499a63664bd49d66e7eed3947c753cc41a7be
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-01 13:10:33 +0330

    connectDb changes

--- src/utils/ConnectDb.js
+++ src/utils/ConnectDb.js
@@ -1,11 +1,11 @@
-import { connect, mongoose } from "mongoose";
+import { mongoose } from "mongoose";
 
 const connectDb = async () => {
   const BASE_URL = process.env.MONGO_URI;
-  mongoose.set('strictQuery', false);
   if (mongoose.connections[0].readyState) return;
+  mongoose.set("strictQuery", false);
   try {
-    await connect(BASE_URL);
+    await mongoose.connect(BASE_URL);
   } catch (error) {
     console.log("can not connect to db", error);
   }
