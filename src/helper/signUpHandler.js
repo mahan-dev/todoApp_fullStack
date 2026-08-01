eb0859c387c56094526f6903d3960881cba980ea
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-01 14:14:38 +0330

    signup changes-7

--- src/helper/signUpHandler.js
+++ src/helper/signUpHandler.js
@@ -7,26 +7,15 @@ export const SignUpHandler = async (form, setLoading) => {
   };
   setLoading(true);
 
-  try {
-    const res = await axios.post("api/auth/sign-up", form);
-    const data = res.data;
+  const res = await axios.post("api/auth/sign-up", form);
+  const data = res.data;
 
-    const success = data.status === "Success";
-    if (success) {
-      toast.success("an Account Created", duration);
-      await new Promise((resolver) => setTimeout(resolver, 2000));
-    }
-
-    return true;
-  } catch (error) {
-    const defaultMessage = "something went wrong";
-    const message = error.response.data?.message;
-    const displayMessage = message ? message : defaultMessage;
-
-    if (error.status === 401) toast.error(displayMessage, duration);
-
-    return false;
-  } finally {
-    setLoading(false);
+  const success = data.status === "Success";
+  if (success) {
+    toast.success("an Account Created", duration);
+    await new Promise((resolver) => setTimeout(resolver, 2000));
   }
+
+  setLoading(false);
+  return true;
 };
