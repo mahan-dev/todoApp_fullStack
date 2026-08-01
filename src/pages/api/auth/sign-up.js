156d4f4a8cbf325654527f8c69439f36782568e2
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-01 13:23:47 +0330

    signup changes-3

--- src/pages/api/auth/sign-up.js
+++ src/pages/api/auth/sign-up.js
@@ -11,6 +11,27 @@ const handler = async (req, res) => {
     });
   try {
     await connectDb();
+    const { email, password } = req.body;
+
+    const existUser = await User.findOne({ email: email });
+    if (existUser) {
+      res.status(401).json({
+        status: "Failed",
+        message: "User already exist's 😢",
+      });
+      return;
+    }
+    const encryptedPassword = await hashPassword(password);
+    const newUser = await User.create({
+      email: email,
+      password: encryptedPassword,
+    });
+
+    res.status(201).json({
+      status: "Success",
+      message: "Successfully created ✔",
+      data: newUser,
+    });
   } catch (err) {
     console.error("DB Error:", err);
 
@@ -19,28 +40,6 @@ const handler = async (req, res) => {
       message: err.message,
     });
   }
-
-  const { email, password } = req.body;
-
-  const existUser = await User.findOne({ email: email });
-  if (existUser) {
-    res.status(401).json({
-      status: "Failed",
-      message: "User already exist's 😢",
-    });
-    return;
-  }
-  const encryptedPassword = await hashPassword(password);
-  const newUser = await User.create({
-    email: email,
-    password: encryptedPassword,
-  });
-
-  res.status(201).json({
-    status: "Success",
-    message: "Successfully created ✔",
-    data: newUser,
-  });
 };
 
 export default handler;
