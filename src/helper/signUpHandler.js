494210b69c40e6162029ef48b8fbcf2094691dfc
mahan.dev <mahansoodbakhsh@yahoo.com>
2026-08-01 13:27:07 +0330

    signup changes-4

--- src/helper/signUpHandler.js
+++ src/helper/signUpHandler.js
@@ -7,13 +7,8 @@ export const SignUpHandler = async (form, setLoading) => {
   };
   setLoading(true);
 
-  const data = await axios.post("api/auth/test", {hi: "there"})
-  console.log(data)
-
-
-  return
   try {
-    const res = await axios.post("/api/auth/sign-up", form);
+    const res = await axios.post("api/auth/sign-up", form);
     const data = res.data;
 
     const success = data.status === "Success";
